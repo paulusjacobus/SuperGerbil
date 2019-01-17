39914bce5558d288a4aee15e2e05c1b1562a935f
paulusjacobus <pauljacobdegroot@gmail.com>
2019-01-17 17:58:56 +1100

    Add files via upload

---
+++ usb/stbool.h
@@ -0,0 +1,31 @@
+/******************** (C) COPYRIGHT 2011 STMicroelectronics ********************
+ * File Name          : hw_config.h
+ * Author             : MCD Application Team
+ * Version            : V3.3.0
+ * Date               : 21-March-2011
+ * Description        : Hardware Configuration & Setup
+ ********************************************************************************
+ * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+ * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
+ * AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+ * CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+ * INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+ *******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+
+
+// file : myboolean.h
+#ifndef STBOOL_H
+#define STBOOL_H
+
+#define false 0
+#define true 1
+typedef int bool; // or #define bool int
+
+
+/* External variables --------------------------------------------------------*/
+
+#endif  /*__HW_CONFIG_H*/
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
